062a93a58cdee6f8652ed4765175db59f39fbae3
plabonislam <shahnurislamplabon@gmail.com>
2024-11-26 20:39:27 +0600

    type checking is added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+let test:string = "hi" 
 </script>
 
 <template>
   <main>
+    <p> {{test}} </p>
     <TheWelcome />
   </main>
 </template>
